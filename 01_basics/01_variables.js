9e927ee05774c0e51b55311d9738842a11d89b2c
Hannath Ayesha <hannathayesha@gmail.com>
2026-08-09 10:07:20 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 422231
+let accountEmail = "hannath@google.com"
+var accountPassword="12345" 
+/*  "var" is not used now , since it has problem in block of scope and functional scope.*/
+accountCity="Jaipur"
+ // this is also a type of declariang variable which is not that good to use.
+
+let accountState; //here we have not stored any value, so when u run , u get undefined.
+//accountID = 2 // not allowed, we cannot change accountId since is it const
+
+accountEmail="h@hc.com"
+accountPassword= "24240541"
+accountCity="Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as new version
+
+//alert( 3 + 3 ) // we are using  nodejs, not browser
+
+console.log(3
+    +3) // this is not a good proctice, code readability should be high
+
+console.log("hannath")
+
+let name="hitesh"// string
+let age = 18 //number 
+let isLoggedIn = false // boolean 
+let state;
+//number => 2 to power 53 
+//bigint 
+// string => ""
+//boolean => true/false
+//null  => standalone value
+// undefined => value is not assigned
+//symbol => unique
+
+//object 
+
+console.log(typeof"hannath");
+
+console.log(typeof undefined);// undefined
+console.log(typeof null);// object
