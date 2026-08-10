2687b765fd9d17cd572d5abb7784cd25c7e43f3e
Hannath Ayesha <hannathayesha@gmail.com>
2026-08-10 13:02:14 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,12 +1,15 @@
 const accountId = 422231
 let accountEmail = "hannath@google.com"
 var accountPassword="12345" 
+
 /*  "var" is not used now , since it has problem in block of scope and functional scope.*/
+
 accountCity="Jaipur"
+
  // this is also a type of declariang variable which is not that good to use.
 
 let accountState; //here we have not stored any value, so when u run , u get undefined.
-//accountID = 2 // not allowed, we cannot change accountId since is it const
+//accountID = 2 // not allowed, we cannot change accountId since is it declared as const
 
 accountEmail="h@hc.com"
 accountPassword= "24240541"
