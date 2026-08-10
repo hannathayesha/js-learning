e9117b1d7b517f4e16af4cae8ed7650f5d744d7d
Hannath Ayesha <hannathayesha@gmail.com>
2026-08-10 12:56:02 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33abc" // if is "33abc" instead of 30 then if u check , it will give NaN
+
+//console.log(typeof score);//string
+//console.log(typeof (score));//string
+
+let valueInNumber = Number(score)//typeconversion
+//console.log(typeof valueInNumber);//Number
+//console.log(valueInNumber)
+
+
+//"33" => 33 
+// "33abc" => NaN 
+//true => 1; false =>0 
+
+let isLoggedIn = 1 
+let booleanIsloggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsloggedIn);
+
+// 1 => true; 0 => false;
+// "" => false 
+// "hannath" => true 
+
+let someNumber = 33 
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
